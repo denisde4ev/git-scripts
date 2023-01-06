335dc8a260540f5cd28c6e6f89b2eee3b222c23b
denisde4ev <git@denisde4ev.xyz>
2023-01-07 00:50:58 +0200

    3

--- gh-shortcut/.update-symlinks.sh
+++ gh-shortcut/.update-symlinks.sh
@@ -1,132 +1,55 @@
 #!/bin/sh
 
 
-unset ghuser
 
-set -eu
-cd "${0%/*}"
-
-pat='/^/ https://github.com/$ghuser/*/raw/master'
+# TODO: REPLACE/MV/MERGE THIS SCRIPT FILE WITH ../denisde4ev-packages/.update-symlinks
 
 
-echo >&2 "$0: not tested but sh. should work.. I hope"
-echo >&2
+
+
+cd "${0%/*}" || exit
+
+
+pat='.../*/raw/master' # not verry configurable, ln still have hardcoded/dup plus `$i`
+
+
 
 unset interactive
 case $1 in --help)
 	case $0 in ./*) o=$0;; *) o=${0##*/}; esac
 
 	printf %s\\n \
-		"Usage: $o [-i] [repo name or path]..." \
+		"Usage: $o [-i]" \
 		"  link folders from pat='$pat' to '\$PWD'='$PWD'/" \
 	;
 	exit
+	;;
+-i)
+	shift
+	interactive=''
 esac
-
 
 
 die() {
-	printf %s\\n " x> %s" "$1"
-	exit "${2-2}"
+	printf %s\\n >&2 "$1"
+	exit ${2-2}
 }
 
 
-case $PWD in 
-"/^/_/git-branch:master/ https:/github.com/"[!/]*/[!/]*)  die "err 1" 1;;
-"/^/_/git-branch:master/ https://github.com/"[!/]*/[!/]*) die "err 2" 1;;
-"/^/_/git-branch:master/ https:/github.com/"*)  pat=${PWD##*/;;
-"/^/_/git-branch:master/ https://github.com/"*) pat=${PWD##*/;;
-
-## not used for now, its plan for in the future:
-#"/_/?branch=master/ https://github.com/"*) pat=${PWD##*/;;
-#'/_/ https://github.com/'*'/#branch=master/') pat=${PWD##*/;;
-
-# huuum... why not just use: (WHY DIDN'T I THINK ABOUT THIS EARLIER :'( ?!)
-# huuum... why not just use: (WHY DIDN'T I THINK OF THIS EARLIER :'( ?!)
-"/^/ https://github.com/denisde4ev/!{repo}!/tree/master/") pat=${PWD##*/;;
-
-# for example: /_/\ https://github.com/denisde4ev/thepkg/#/branch=master/') pat=${PWD##*/;;
-*)
-	printf %s\\n%s >&2 \
-		'Can not auto detect GH user, please provide username" \
-		" (empty for basename of PWD) (or ^C to quit): ' \
-	;
-	read -r pat
-	case pat in ^C) exit 1; esac # I did not ment you to type it, but I will accept it anyway.
+case $# in 0) ;; *)
+	die "too many args, see --help" 2
 esac
 
-ln_it() {
-	j=${1%/raw/master}
-	j=${j##*/}
-	[ ! -e "$j" ] || {
-		printf %s\\n "link $j already exists." >&2
-		return
-	}
-	ln -snT -v${interactive+i} -- ".../$j/raw/master" ./"$j"
-}
 
+IFS=''
 
-exists() { [ -e "$1" ]; }
+testTwoArgs() { test "$1" "$2"; } # use test by only 2 args, reason: do test patten expension that can result in many args
 
+testTwoArgs -d $pat || die "not a dir: '$pat'" 3
 
-case $# in 0)
-	IFS=''
-
-	exists $pat || {
-		printf %s\\n "no files in pat='$pat'"
-		exit
-	}
-	
-	for i in $pat; do
-		ln_it "$i" || :
-	done
-	exit
-esac
-# else: provided arguments:
-
-
-
-# Ahh.. this case with provided arguments
-# will raely be used.
-# Have I overdone it?
-# Nahh..
-#
-# ah yes I did't even tought thsi scritp is going to be used in scripts
-# now.. I have added a -i option for that ..
-
-
-YN() {
-	case ${interactive+i} in
-		i) YN_confirm --printf y %s\\n "$1." "Do you want to link it?";;
- 		*) printf %s\\n "$1" >&2; return 6;; # just a note: exit code 5 is when YN_confirm can't read input
-	esac
-}
-
-
-x=1 # when nothing is created will exit with 1
-for i; do
-
-	case $i in 
-		$pat) ;;
-		*/*)
-			case ${interactive-0} in 0)
-				YN "arg: '$i' is not in expected pattern: '$pat'" \
-				|| continue
-			esac
-		;;
-		*) i="/^/ https://github.com/$ghuser/$i/raw/master";;
-	esac
-
-	[ -d "$i/.git" ] || {
-		if [ -d "$i" ]; then
-			YN "Dir i='$i' does not exist"
-		else
-			YN "Dir i='$i' is not a git repository (./.git folder does not exist)"
-		fi || continue
-	}
-
-	ln_it "$i"
-	x=$?
-
+for i in $pat; do
+	i=${i%/raw/master}
+	i=${i##*/}
+	[ ! -L "$i" ] || continue
+	ln -snT -v${interactive+i} -- ".../$i/raw/master" ./"$i"
 done
-exit "$x"
